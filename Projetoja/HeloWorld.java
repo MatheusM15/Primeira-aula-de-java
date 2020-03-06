d1ed87d832fe2ddf91df88522d5d291c2d3161d0
Matheus <Matheusmenezesribeiro@hotmail.com>
2020-03-06 20:32:10 -0300

    Primeira aula de java

---
+++ Projetoja/HeloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva a descrição da classe HeloWorld aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class HeloWorld
+{
+    public static void main(String[] args){
+     System.out.println("Hello world");  
+    }
+    
+}
